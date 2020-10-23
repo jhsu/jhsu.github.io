c63e175627a287a63c78c6f0d89035809b43cd73
Joe Hsu <jhsu.x1@gmail.com>
2020-10-23 02:30:45 -0400

    start layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import React, { lazy, Suspense }  from 'react';
 import {importMDX} from 'mdx.macro';
+import {MDXProvider} from '@mdx-js/react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import './App.css';
-const Content = lazy(() => importMDX('./posts/001-first-post/Content.mdx'))
 
+import Header from './layout/Header';
+
+const Content = lazy(() => importMDX('./posts/001-first-post/Content.mdx'))
 
 function App() {
   return (
-    <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Content />
-      </Suspense>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <MDXProvider>
+          <Switch>
+            <Route>
+              <Suspense fallback={<div>Loading...</div>}>
+                <Content />
+              </Suspense>
+            </Route>
+          </Switch>
+        </MDXProvider>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/layout/Header.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Header = () => {
+    return <h1><a href="/">joe hsu</a></h1>
+};
+export default Header;
